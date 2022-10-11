7e849392b52ef31d2bf819cfe7f9b73ce4e1150d
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-11 11:05:35 +0600

    header section added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import "./Header.css";
+import headerImage from "../../assets/header.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container fluid className="px-0 position-relative">
+      <img src={headerImage} alt="" className="w-100" />
+      <div className="header-content">
+        <h1 className="text-center fw-bolder">Participate Our Quiz</h1>
+        <h4>
+          JS Quizo Brings An Awesome Opportunity for You. Take our quiz today and judge your skills.
+        </h4>
+        <h4>
+          We have 4 different topics for quiz. You can check your answer after completing the quiz.
+        </h4>
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import "./Home.css";
+import Header from "../Header/Header";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="px-0">
+      <Header />
+      <Container></Container>
+    </div>
+  );
 };
 
 export default Home;
