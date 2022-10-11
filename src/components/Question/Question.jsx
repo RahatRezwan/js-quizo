522370061f33fdf91d348248da609dbf6ed19f57
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-11 07:00:12 +0600

    Create all necessary components and folders

---
+++ src/components/Question/Question.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Question.css";
+const Question = () => {
+  return <div>Question</div>;
+};
+
+export default Question;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Statistics.css";
+const Statistics = () => {
+  return <div>Statistics</div>;
+};
+
+export default Statistics;
