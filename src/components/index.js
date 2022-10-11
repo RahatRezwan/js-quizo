946018928da1eab8b099d60868288bf14aa0f8bb
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-11 07:03:21 +0600

    create index.js inside components and export all components from this sigle file

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar/Navbar";
+import Home from "./Home/Home";
+import Header from "./Header/Header";
+import Article from "./Article/Article";
+import Blogs from "./Blogs/Blogs";
+import Questions from "./Questions/Questions";
+import Question from "./Question/Question";
+import Statistics from "./Statistics/Statistics";
+
+export { Navbar, Home, Header, Article, Blogs, Question, Questions, Statistics };
